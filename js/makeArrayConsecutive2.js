8d8686bd6c1b0a6bca347568d2ec36d56667766c
0x4bd0 <abdoumjr@gmail.com>
2021-01-04 14:49:07 +0100

    sync

--- js/makeArrayConsecutive2.js
+++ js/makeArrayConsecutive2.js
@@ -1,5 +1,4 @@
 function makeArrayConsecutive2(statues) {
-    let count = 0
     let sorted_statues = statues.sort((a,b)=>a-b)
     let filled_statuses = new Array(sorted_statues[sorted_statues.length-1]-sorted_statues[0])    
     return (filled_statuses.length-sorted_statues.length)+1
