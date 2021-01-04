bc286b5dad997530979ce15765d788f888613c99
0x4bd0 <abdoumjr@gmail.com>
2021-01-04 14:28:26 +0100

    sync

--- js/adjacentElementsProduct.js
+++ js/adjacentElementsProduct.js
@@ -3,7 +3,6 @@ function adjacentElementsProduct(inputArray) {
     let most = Number.NEGATIVE_INFINITY
     
     for(let i=0;i<=inputArray.length-2;i++){
-        console.log(inputArray.slice(i,i+2))
         
         let product = inputArray.slice(i,i+2).reduce((a,b)=>a*b)
         
