47529a8151015f82164cecd6a1365190d2af0cde
0x4bd0 <abdoumjr@gmail.com>
2021-01-05 14:47:47 +0100

    arrayChange

---
+++ js/arrayChange.js
@@ -0,0 +1,16 @@
+function arrayChange(inputArray) {
+    let total = 0
+  
+  
+     for(let i = 1; i < inputArray.length ; i++){
+         if(inputArray[i]<=inputArray[i-1]){
+             let diff = inputArray[i-1] - inputArray[i] +1
+             inputArray[i] += diff
+             total += diff
+         }
+     }
+     
+      return total
+ 
+ }
+ 
