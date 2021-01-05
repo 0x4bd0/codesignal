5ac5ca3e0d6abb8af2f33e05d20f12cc6d5bcf04
0x4bd0 <abdoumjr@gmail.com>
2021-01-05 17:20:17 +0100

    isIPv4Address

---
+++ js/isIPv4Address.js
@@ -0,0 +1,8 @@
+function isIPv4Address(inputString) {
+    let arr = inputString.split('.')
+    
+    if(arr.length !== 4) return false
+    
+    return !arr.some(item=> (item[0] == 0 && item.length > 1) || item.length == 0 || isNaN(item) || item > 255)
+        
+}
