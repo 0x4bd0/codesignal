deb293d08247437d9d897988a60c03ca47e3757f
0x4bd0 <abdoumjr@gmail.com>
2021-01-07 15:50:45 +0100

    absoluteValuesSumMinimization

---
+++ js/absoluteValuesSumMinimization.js
@@ -0,0 +1,24 @@
+function absoluteValuesSumMinimization(a) {
+    
+    let tmp = []
+     
+    for(let i = 0; i < a.length; i++){
+       let diff = 0
+     for(let j = 0; j < a.length; j++){
+         diff += Math.abs(a[j] - a[i])
+     }
+     
+     tmp.push({
+         elem : a[i],
+         value : diff
+     })
+     
+    }
+    
+    let result = tmp.sort((a,b)=>a.value>b.value ? -1 : b.value > a.value ? 1 : 0)
+    
+    result = result.filter(item=>item.value == result[result.length-1].value)
+    
+    return result.length > 1 ? result.sort((a,b)=>a.elem>b.elem ? 1 : b.elem > a.elem ? -1 : 0)[0].elem : result[0].elem
+
+}
