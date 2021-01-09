6653648f29d3a8f6867351c01a76b5da49e6b7e0
0x4bd0 <abdoumjr@gmail.com>
2021-01-09 14:04:25 +0100

    isBeautifulString

---
+++ js/isBeautifulString.js
@@ -0,0 +1,17 @@
+function isBeautifulString(inputString) {
+    let alpha = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let arr = inputString.split('')
+    let unique = arr.filter((value, index, self) => self.indexOf(value) === index).sort((a,b)=>alpha.indexOf(a)-alpha.indexOf(b))
+            for(let i = unique.length; i > 0 ; i--){
+    
+                let currentIndex = alpha.indexOf(unique[i])
+                let nextIndex = alpha.indexOf(unique[i-1]) 
+    
+            if(alpha[nextIndex-1] !== undefined && unique.indexOf(alpha[nextIndex-1]) == -1) return false
+            
+       if(((currentIndex - nextIndex ) >= 1) && ((arr.filter(item=>item==unique[i]).length - arr.filter(item=>item==unique[i-1]).length) > 0)) return false
+       
+            }
+            return true
+    }
+    
