5008bf80e9bf973185a796e5601279fa3be4e13e
abdelkader MOUDJAR <abdoumjr@gmail.com>
2021-01-15 12:42:51 +0100

    Create securityBreach.sql

---
+++ Databases/securityBreach.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE securityBreach()
+BEGIN
+	SELECT first_name,second_name,attribute
+    FROM users
+    WHERE BINARY attribute LIKE  CONCAT('_%', CONCAT('\%',first_name, '\_',second_name,'\%'), '%');
+END
