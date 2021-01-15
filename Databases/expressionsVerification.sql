30627a3192efc89718d8afec9f3e7520636eb69f
0x4bd0 <abdoumjr@gmail.com>
2021-01-15 16:49:18 +0100

    expressionsVerification

---
+++ Databases/expressionsVerification.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE expressionsVerification()
+BEGIN
+	SELECT *
+    FROM expressions
+    WHERE CASE operation
+    WHEN '+' THEN a+b=c
+    WHEN '-' THEN a-b=c
+    WHEN '*' THEN a*b=c
+    WHEN '/' THEN a/b=c
+    END;
+END
---
+++ Databases/x.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE newsSubscribers()
+BEGIN
+    SELECT subscriber FROM half_year
+    WHERE newspaper LIKE "%Daily%"
+    UNION SELECT subscriber FROM full_year
+    WHERE newspaper LIKE "%Daily%"
+    ORDER BY subscriber ASC;
+END
