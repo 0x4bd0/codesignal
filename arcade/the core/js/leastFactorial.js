2465fc565968298c72bde20b634dfcf8de010197
0x4bd0 <abdoumjr@gmail.com>
2021-01-16 20:27:17 +0100

    leastFactorial

---
+++ arcade/the core/js/leastFactorial.js
@@ -0,0 +1,11 @@
+const leastFactorial = n => {
+    if(n == 1) return 1
+    let count = 1
+    let i = 1
+    while(n > count){
+        count*=i
+        i++
+    }
+    return count
+}
+guit git dzdzdzzd
